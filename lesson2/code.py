8f98ae86e69a8faf7c1418c5d97dbae8d2b3e44c
wajehulhasan <70309461+wajehulhasan@users.noreply.github.com>
2024-09-25 12:41:25 +0500

    Create code.py

---
+++ lesson2/code.py
@@ -0,0 +1,47 @@
+# now you can write anything here
+'''
+now this is all comments
+as
+fa
+dfgad
+ga
+dgad
+
+'''
+
+# if i wantr to make this a comment 
+
+print('you text to be printed')
+print('this is the first part',345635)
+a = 46
+print('the value of a:a')
+print('the value of a:',a)
+print('the value of a:',a,'and the alue of b is not gien')
+print(f'the value of a:{a}')
+print(f' the value of a:{a} and the value of b is not given')
+
+#  data types
+
+A = 35
+FLOAT = .25
+COMPLEX = complex(12,134)
+BOOLEAN = True
+STRING = 'Wajeh'
+LIST = [1,3,6,'wajeh',.25]
+TUPLE = (4,9,'hasan',34)
+DICTIONARY = {"NAME": 'WAJEH UL HASAN', 'age': 34}
+
+print(type(A))
+print(type(FLOAT))
+print(type(COMPLEX))
+print(type(BOOLEAN))
+print(type(LIST))
+print(type(TUPLE))
+print(type(DICTIONARY))
+
+A = '2'
+B = '4'
+
+print(A+B)
+
+
